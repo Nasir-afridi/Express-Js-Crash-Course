01444ebf79644f2bfcdc3343952526b733586c84
Nasir-afridi <nasirwc05@gmail.com>
2025-01-12 12:51:01 +0500

    changed the content type and status code

--- first.js
+++ first.js
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 
 app.get ('/healthy', (req, res) => {
-    res.send ('healthy users!')
+    //res.send ('healthy users!');
+    res.status(400).json({message:"hello"})     //changing the status code on 400 it will be bad request.
 })
 
 const PORT = process.env.PORT || 4000;
